22a3f9e5ee65726b0683baf4dc2a0e75d5f244da
Sree Raghu Paruchuri <raghu4466@hotmail.com>
2016-09-25 23:25:52 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,11 @@
+var app = angular.module("gettingStarted", []);
+
+app.controller("gtgStdCtrl", Addnew);
+
+
+function Addnew($scope) {
+
+  console.log("AddNew");
+  var addNewTable = document.getElementById("AddNewTable");
+  
+}
