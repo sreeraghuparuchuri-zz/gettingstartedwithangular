25ccc65340e47bdc1392371b1c6a18d764a6290a
Sree Raghu Paruchuri <raghu4466@hotmail.com>
2016-09-28 09:20:29 +0530

    Read Values from DOM Elements
    Added submitAddNew method in app.js and modified index.html.

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 var app = angular.module("gettingStarted", []);
 
-app.controller("gtgStdCtrl", function($scope) {
-console.log($scope);
-$scope.showMe = false;
-    $scope.Addnew = function() {
+app.controller("gtgStdCtrl", function ($scope) {
+    console.log($scope);
+    $scope.showMe = false;
+    $scope.addNew = function () {
         $scope.showMe = !$scope.showMe;
     }
-    
-    $scope.newFunc = function() {
-    console.log("HEllo");
-    
+
+    $scope.submitAddNew = function () {
+        console.log("submitAddNew");
+
+        var fName = document.getElementsByName("fname")[0].value;
+        var lName = document.getElementsByName("lname")[0].value;
+        var age = document.getElementsByName("age")[0].value;
+
+        console.log(fName);
+        console.log(lName);
+        console.log(age);
+
+        //$scope.showMe = false;
+
     }
+
 });
