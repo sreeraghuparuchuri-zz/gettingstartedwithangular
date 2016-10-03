198f02359fb1fb5688ce6c3724417d9d99507cfb
Sree Raghu Paruchuri <raghu4466@hotmail.com>
2016-10-03 09:20:28 +0530

    Update the Info table.
    Changes to index.html to add a new controller and binding to the table.
    
    Changes to app.js to create a new controller and function to fetch the
    data from the database.

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ app.controller("gtgStdCtrl", function ($scope, $http) {
         console.log(lName);
         console.log(age);
 
-        var req = {
-            method: "POST",
+        var putReq = {
+            method: "PUT",
             url: "http://localhost:8080/com.raghu.webservice.restful/gettingstarted/addnew",
             headers: {
                 'Content-Type': 'application/json'
@@ -31,12 +31,59 @@ app.controller("gtgStdCtrl", function ($scope, $http) {
             }
         }
 
-        $http(req).then(function (response) {
+        $http(putReq).then(function (response) {
             console.log(response);
 
         });
         //$scope.showMe = false;
 
     }
+
+    
+
+});
+
+app.controller("gtgStdDataCtrl", function($scope, $http) {
+
+$scope.getData = function() {
+   // alert("inside getData");
+
+// $http({
+//   method: 'GET',
+//   url: "http://localhost:8080/com.raghu.webservice.restful/gettingstarted/hello"
+// }).then(function successCallback(response) {
+//     // this callback will be called asynchronously
+//     // when the response is available
+//     console.log("successCallBack",response);
+//   }, function errorCallback(response) {
+//     // called asynchronously if an error occurs
+//     // or server returns response with an error status.
+//     console.log("errorCallBack",response);
+//   });
+
+
+var getReq = {
+            method: "GET",
+            url: "http://localhost:8080/com.raghu.webservice.restful/gettingstarted/hello",
+            
+        }
+
+        $http(getReq).then(function (response) {
+            console.log(response);
+           angular.forEach(data, function(value, key) {
+               console.log("Each Response:::",response);
+              // console.log(value);
+               //console.log(key);
+
+           });  
+
+            // console.log(response.data);
+            
+            // var splitResponse = response.data.split(",");
+            // console.log(splitResponse[3]);
+
+        });
+
+}
 
 });
